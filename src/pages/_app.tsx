e0cfdb4def3a5a401478f293e1bd45da41eca032
Sergio Brener <sergio.brener95@gmail.com>
2021-03-01 15:37:51 -0300

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/global.css'
+
+import { ChallengesProvider } from '../contexts/ChallengesContext'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default MyApp
